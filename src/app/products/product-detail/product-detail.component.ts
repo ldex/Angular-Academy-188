1efff6190cc533eabb7d2fa18f7158ad3bd1b0ba
ldex <laurent.duveau+git@gmail.com>
2020-03-26 16:36:00 -0400

    End of Day 1

---
+++ src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product.interface';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  @Input() product: Product;
+
+  constructor() { }
+
+  IsEditable(product: Product) {
+    return product.fixedPrice || product.price < 500
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.interface';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  title: string = 'Products';
+  products: Product[];
+  selectedProduct: Product;
+
+  onSelect(product: Product) {
+    this.selectedProduct = product;
+  }
+
+  constructor(private productService: ProductService) { 
+   
+  }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+
+}
