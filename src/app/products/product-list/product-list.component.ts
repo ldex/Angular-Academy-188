a088daa4b237da764d59e7ee72c2c154680d2a43
ldex <laurent.duveau+git@gmail.com>
2020-03-28 08:03:20 -0400

    End of Day 2

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.interface';
 import { ProductService } from '../product.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
@@ -10,8 +12,30 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
 
   title: string = 'Products';
-  products: Product[];
+ // products: Product[];
+  products$: Observable<Product[]>;
   selectedProduct: Product;
+  productsNb: number = 0;
+
+  // Pagination
+  pageSize: number = 5;
+  start: number = 0;
+  end: number = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.selectedProduct = null;
+    this.currentPage--;
+  }
+
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.selectedProduct = null;
+    this.currentPage++;
+  }
 
   onSelect(product: Product) {
     this.selectedProduct = product;
@@ -22,7 +46,20 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+
+    this.products$ = this
+                      .productService
+                      .products$
+                      .pipe(
+                        tap(products => this.productsNb = products.length)
+                      );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     results => this.products = results
+    //   );
   }
 
 }
